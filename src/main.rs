417c2d11af90cdff0e3ad10fd64f2147de2c058d
A. Taha Baki <atahabaki@pm.me>
2023-11-14 19:12:29 +0300

    add: warp::serve /traveler & /api/plane

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 pub mod defaults;
+pub mod filters;
+pub mod handlers;
 pub mod models;
 
 use defaults::get_defaults;
-use serde::{Serialize, Deserialize};
+use sqlx::postgres::PgPoolOptions;
 use warp::Filter;
-
-#[derive(Serialize, Deserialize)]
-struct SearchQuery {
-    realm: String,
-}
 
 #[tokio::main]
 async fn main() {
-    let search = warp::path("travel")
-        .and(warp::query::<SearchQuery>())
-        .map(|sq: SearchQuery| format!("{}", sq.realm));
-    warp::serve(search).run(get_defaults()).await;
+    let db_url = std::env::var("DATABASE_URL").expect("ERR: DATABASE_URL env var must be set.");
+    let pool = PgPoolOptions::new()
+        .max_connections(8)
+        .connect(&db_url)
+        .await
+        .unwrap_or_else(|_| panic!("ERR: failed to connect to database."));
+    let traveler_asks = filters::traveler_asks();
+    let astral_planes = filters::api::apis(pool);
+    let routes = traveler_asks.or(astral_planes);
+    warp::serve(routes).run(get_defaults()).await;
 }
